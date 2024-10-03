461f55b81a90979d97c187c41492e4603e897d0b
Yipmong Said <Syipmong5@gmail.com>
2024-10-03 22:32:01 +0100

    Add initial code files

---
+++ app.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const os = require('os');
+const fs = require('fs');
+const EventEmitter = require('events');
+
+const emitter = new EventEmitter();
+
+
+
+
+
+
+
+
---
+++ logger.js
@@ -0,0 +1,10 @@
+console.log(__filename)
+console.log(__dirname)
+
+var url = 'http://mylogger.io/log';
+
+function log(message){
+    console.log(message)
+}
+
+module.exports = log
